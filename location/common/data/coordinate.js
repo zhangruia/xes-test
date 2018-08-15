c2bc40a524a89bc06d8bf06682da04cace049a8f
yanxipeng <18367618712@163.com>
2018-08-15 15:40:36 +0800

    coordinate文件

---
+++ location/common/data/coordinate.js
@@ -0,0 +1,35 @@
+export var coordinates = [
+    //选择题数据
+    {
+      spaceX: 400,
+      spaceY: 0,
+      contentX: 300,
+      contentY: 660,
+      rightX: 0,
+      modelType: 1},
+    //填空题数据
+   {
+      spaceX: 0,
+      soaceY: 0,
+      contentX: 0,
+      contentY: 0,
+      rightX: 0,
+      modelType: 2},
+    //分类题
+    {
+      spaceX: 0,
+      soaceY: 0,
+      contentX: 0,
+      contentY: 0,
+      rightX: 0,
+      modelType: 3},
+    //连线题数据
+   {
+      spaceX: 0,//x轴图片间距
+      spaceY: 200,//y轴图片间距
+      contentX:360,//x容器坐标
+      contentY:130,//y容器坐标
+      rightX: 1300,
+      modelType: 4} //题型
+  ]
+//   export default {coordinate}
